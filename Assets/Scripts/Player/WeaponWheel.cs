af69489c4f2dde1731bdc3564ca96e819095aa57
Guilherme Figueira <g.figueira.2002@gmail.com>
2024-05-07 11:28:55 +0100

    progress

--- Assets/Scripts/Player/WeaponWheel.cs
+++ Assets/Scripts/Player/WeaponWheel.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class WeaponWheel : MonoBehaviour
 {
@@ -26,15 +28,16 @@ public class WeaponWheel : MonoBehaviour
     //[SerializeField] private GameObject hammer;
     
     public Weapon HeldWeapon {get; private set;} = Weapon.Revolver;
+    private GameObject _uiGameObjectBeingHovered;
 
-    private int UILayer;
+    private int _uiLayer;
     private float _initSensitivity;
-    private bool _afterOpeningWeaponWheel = false;
+    private bool _afterOpeningWeaponWheel;
     
     void Start()
     {
         _inputManager = InputManager.Instance;
-        UILayer = LayerMask.NameToLayer("UI");
+        _uiLayer = LayerMask.NameToLayer("UI");
         _initSensitivity = _virtualCamera.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed;
         _startTimeScale = Time.timeScale;
         _startFixedDeltaTime = Time.fixedDeltaTime;
@@ -42,11 +45,19 @@ public class WeaponWheel : MonoBehaviour
 
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            float y = revolver.transform.position.y;
+            revolver.transform.DOMoveY(y - 1.207f, 2f);
+        }
+        
         var pov = _virtualCamera.GetCinemachineComponent<CinemachinePOV>();
         if (_inputManager.IsWeaponWheelOut())
         {
             weaponWheel.SetActive(true);
             StartSlowMotion();
+            CheckSelectedWeapon();
             pov.m_HorizontalAxis.m_MaxSpeed = 0;
             pov.m_VerticalAxis.m_MaxSpeed = 0;
             Cursor.lockState = CursorLockMode.None;
@@ -57,7 +68,7 @@ public class WeaponWheel : MonoBehaviour
         {
             if (!_afterOpeningWeaponWheel) return;
             StopSlowMotion();
-            HeldWeapon = GetSelectedWeapon();
+            HeldWeapon = CheckSelectedWeapon();
             ChangeWeapon(HeldWeapon);
             Debug.Log(HeldWeapon);
             weaponWheel.SetActive(false);
@@ -71,38 +82,67 @@ public class WeaponWheel : MonoBehaviour
     
     private void ChangeWeapon(Weapon newWeapon)
     {
-        revolver.SetActive(false);
-        sword.SetActive(false);
-        //hammer.SetActive(false);
-        switch (newWeapon)
+        if (revolver.activeSelf)
         {
-            case Weapon.Revolver:
-                revolver.SetActive(true);
-                break;
-            case Weapon.Sword:
-                sword.SetActive(true);
-                break;
-            case Weapon.Hammer:
-                //hammer.SetActive(true);
-                break;
-            default:
-                break;
+            float currentRevolverY = revolver.transform.position.y;
+            revolver.transform.DOMoveY(currentRevolverY - 1.207f, 1f);
+            float currentSwordY = sword.transform.position.y;
+            sword.transform.DOMoveY(currentSwordY + 1.207f, 1f);
         }
+        else
+        {
+            float currentSwordY = sword.transform.position.y;
+            sword.transform.DOMoveY(currentSwordY - 1.207f, 1f);
+            float currentRevolverY = revolver.transform.position.y;
+            revolver.transform.DOMoveY(currentRevolverY + 1.207f, 1f);
+        }
+        //revolver.SetActive(false);
+        //sword.SetActive(false);
+        ////hammer.SetActive(false);
+        //switch (newWeapon)
+        //{
+        //    case Weapon.Revolver:
+        //        revolver.SetActive(true);
+        //        break;
+        //    case Weapon.Sword:
+        //        sword.SetActive(true);
+        //        break;
+        //    case Weapon.Hammer:
+        //        //hammer.SetActive(true);
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
     
-    public Weapon GetSelectedWeapon()
+    public Weapon CheckSelectedWeapon()
     {
         return PointerOverUIElement(GetEventSystemRaycastResults());
     }
- 
+    
  
     private Weapon PointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
     {
-        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        for (int index = 0; index < eventSystemRaysastResults.Count; )
         {
             RaycastResult curRaysastResult = eventSystemRaysastResults[index];
-            if (curRaysastResult.gameObject.layer == UILayer)
-                switch (curRaysastResult.gameObject.tag)
+            var curGameObject = curRaysastResult.gameObject;
+            if (curGameObject.layer == _uiLayer)
+            {
+                if(_uiGameObjectBeingHovered != null)
+                    _uiGameObjectBeingHovered.GetComponent<Outline>().enabled = false;
+                _uiGameObjectBeingHovered = curGameObject;
+                Outline outline = _uiGameObjectBeingHovered.GetComponent<Outline>();
+                if (outline == null)
+                {
+                    var parent = _uiGameObjectBeingHovered.transform.parent;
+                    outline = parent.GetComponent<Outline>();
+                    _uiGameObjectBeingHovered = parent.gameObject;
+                }
+                
+                outline.enabled = true;
+                
+                switch (curGameObject.tag)
                 {
                     case "RevolverUI":
                         return Weapon.Revolver;
@@ -113,6 +153,7 @@ public class WeaponWheel : MonoBehaviour
                     default :
                         return Weapon.None;
                 }
+            }
         }
         return Weapon.None;
     }
