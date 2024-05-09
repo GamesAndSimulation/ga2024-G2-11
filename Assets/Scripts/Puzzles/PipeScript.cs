94d7a06ea8ece4bd4a7b4abe1d73bb88f8b8c476
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-05-09 14:16:47 +0100

    Pipe game started

---
+++ Assets/Scripts/Puzzles/PipeScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzle : MonoBehaviour 
+{
+
+    float[] rotations = { 0, 90, 180, 270 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        transform.Rotate(new Vector3(0, 0, 90));
+    }
+}
