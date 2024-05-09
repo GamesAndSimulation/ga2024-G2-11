db8ca0eb2edad12f343a06923676940ba9d0e4a4
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-05-09 15:45:59 +0100

    Some more updates to the pipe game
    Checking if everything is correct missing

--- Assets/Scripts/Puzzles/PipeScript.cs
+++ Assets/Scripts/Puzzles/PipeScript.cs
@@ -7,6 +7,9 @@ public class PipePuzzle : MonoBehaviour
 
     float[] rotations = { 0, 90, 180, 270 };
 
+    public float correctRotation;
+    private bool isPlacedCorrectly = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +26,7 @@ public class PipePuzzle : MonoBehaviour
     void OnMouseDown()
     {
         transform.Rotate(new Vector3(0, 0, 90));
+        if (transform.eulerAngles.z == correctRotation)
+            isPlacedCorrectly = true;
     }
 }
