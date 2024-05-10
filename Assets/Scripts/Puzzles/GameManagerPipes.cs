e080956a7345feacd57105c1b2717d23004f5d84
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-05-10 15:24:46 +0100

    Pipe game finished

---
+++ Assets/Scripts/Puzzles/GameManagerPipes.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+
+public class GameManagerPipes : MonoBehaviour
+{
+    public GameObject PipesHolder;
+    public GameObject[] Pipes;
+
+    public int criticalPathPipes = 0;
+    public int correctPipes = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        criticalPathPipes = PipesHolder.transform.childCount;
+        Pipes = new GameObject[criticalPathPipes];
+
+        for (int i = 0; i < Pipes.Length; i++)
+        {
+            Pipes[i] = PipesHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (correctPipes == criticalPathPipes)
+            Debug.Log("You won the game");
+    }
+
+    public void AddCorrectPipe()
+    {
+        correctPipes++;
+        Debug.Log("Added pipe");
+    }
+
+    public void RemoveCorrectPipe()
+    {
+        correctPipes--;
+        Debug.Log("Removed pipe");
+    }
+}
--- Assets/Scripts/Puzzles/PipeScript.cs
+++ Assets/Scripts/Puzzles/PipeScript.cs
@@ -4,35 +4,64 @@ using UnityEngine;
 
 public class PipePuzzle : MonoBehaviour 
 {
-
-    float[] rotations = { 0, 90, 180, 270 };
+    private readonly float[] _rotations = { 0, 90, 180, 270 };
+    private bool _isPlacedCorrectly = false;
 
     public float[] correctRotation;
-    private bool isPlacedCorrectly = false;
+    public bool isPartOfSolution = false;
+
+    public GameManagerPipes gameManagerPipeGameObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        int rand = Random.Range(0, rotations.Length);
-        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        gameManagerPipeGameObject = GameObject.Find("GameManagerPipes").GetComponent<GameManagerPipes>();
+            
+        int rand = Random.Range(0, _rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, _rotations[rand]);
+
+        if (isPartOfSolution)
+        {
+            // Check if the pipe is correctly placed from the start
+            for (int i = 0; i < correctRotation.Length; i++)
+            {
+                if (transform.eulerAngles.z == correctRotation[i])
+                {
+                    _isPlacedCorrectly = true;
+                    gameManagerPipeGameObject.AddCorrectPipe();
+                    break;
+                }
+            }
+        }
     }
 
     void OnMouseDown()
     {
         transform.Rotate(new Vector3(0, 0, 90));
 
-        for (int i = 0; i < correctRotation.Length; i++)
+        if (isPartOfSolution)
         {
-            if (transform.eulerAngles.z == correctRotation[i])
-                isPlacedCorrectly = true;
+
+            for (int i = 0; i < correctRotation.Length; i++)
+            {
+                // If it was previously placed correctly, and now it's not, remove the flag
+                if (_isPlacedCorrectly)
+                {
+                    _isPlacedCorrectly = false;
+                    gameManagerPipeGameObject.RemoveCorrectPipe();
+                }
+
+                // If it is placed correctly, note that
+                if (transform.eulerAngles.z == correctRotation[i])
+                {
+                    _isPlacedCorrectly = true;
+                    gameManagerPipeGameObject.AddCorrectPipe();
+                    break;
+                }
+            }
         }
-        
-        
+
+
     }
 }
