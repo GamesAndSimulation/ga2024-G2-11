b894ae51c3f794513ab7c9df22a4e143decf3bd6
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-05-13 19:47:07 +0100

    Fixed something but tbh, might be better to call a coroutine

--- Assets/Scripts/Puzzles/GameManagerPipes.cs
+++ Assets/Scripts/Puzzles/GameManagerPipes.cs
@@ -8,6 +8,7 @@ public class GameManagerPipes : MonoBehaviour
     public GameObject PipesHolder;
     public GameObject[] Pipes;
     public GameObject Gear;
+    public ParticleSystem steamSystem;
 
     public int criticalPathPipes = 0;
     public int correctPipes = 0;
@@ -25,12 +26,16 @@ public class GameManagerPipes : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (correctPipes == criticalPathPipes)
         {
-            Debug.Log("You won the game");
+            //Debug.Log("You won the game");
             Gear.transform.Rotate(0, 0, 5,  Space.Self);
+            Debug.Log(Gear.transform.eulerAngles);
+            
+            if (!steamSystem.isStopped)
+                steamSystem.Stop();
             
         }
     }
