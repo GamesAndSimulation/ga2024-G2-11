33346fd005867d5e115d5aa200be1601b87a3408
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-05-30 21:35:28 +0100

    Added main menu
    Hover doesn't work tho ;-;

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
+
+public class MenuScript : MonoBehaviour
+{
+    public Canvas mainMenuCanvas;
+    public PlayableDirector introScene;
+
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void play()
+    {
+        mainMenuCanvas.gameObject.SetActive(false);
+        Debug.Log("Play clicked");
+        introScene.Play();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+    
+    void OnGUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    
+    public void options()
+    {
+        Debug.Log("Options clicked");
+    }
+    
+    public void quit()
+    {
+        Debug.Log("Quit clicked");
+        Application.Quit();
+    }
+}
