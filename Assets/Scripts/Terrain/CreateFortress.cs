f2ac109d99a932d78f79324334c9e23da49ecad7
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-06-04 00:03:12 +0100

    Added obstacles to the fortress

--- Assets/Scripts/Terrain/CreateFortress.cs
+++ Assets/Scripts/Terrain/CreateFortress.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class CreateOutpost : MonoBehaviour
 {
-  
-    Terrain[] _terrains;
     
     // ----------- Constants -----------
     // Walls
@@ -58,61 +56,53 @@ public class CreateOutpost : MonoBehaviour
 
     private float heightOffset = 0;
     private float rotationOffset = 0;
+
+    [Range(0, 10)]
+    public int numberOfObstacles = 5;
     
     // Start is called before the first frame update
     void Start()
     {
-        _terrains = Terrain.activeTerrains;
-        
-        // if no initial position specified, use zero, otherwise, use the object position
-        if (!useSpecifiedStartingPosition)
-            specifiedObjectPosition = Vector3.zero;
-        else
-            specifiedObjectPosition = specifiedObject.transform.position;
+        // If no initial position specified, use zero, otherwise, use the object position
+        specifiedObjectPosition = !useSpecifiedStartingPosition ? Vector3.zero : specifiedObject.transform.position;
 
-        assets = new GameObject[]
-            { simpleWall, strongWall, strongWallDoor, simpleCorner, strongCorner, strongEnd, tower, destructibleWall };
+        assets = new[] { simpleWall, strongWall, strongWallDoor, simpleCorner, strongCorner, strongEnd, tower, destructibleWall };
 
         
         Build();
         RepositionBuilding();
+        GenerateObstacles();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
-    void Build()
+    private void Build()
     {
         
-        // Position corners
+        // ----------- Position Corners -----------
         for (var x = 0; x < 2; x += 1)
         {
             for (var z = 0; z < 2; z += 1)
             {
-                
+                // Calculate position and rotation
                 Vector3 position = new Vector3(specifiedObjectPosition.x + x * xWidth, specifiedObjectPosition.y, specifiedObjectPosition.z + z * zLength);
                 Vector3 rotation = new Vector3(0, (x + z + 2) * 90, 0);
                 
+                // Set the rotation for the "special corner"
                 if (z == 0 && x == 1)
                     rotation = new Vector3(0, 90, 0);
             
+                // Create the object instance and set its parent to the specified one
                 GameObject myInstance = Instantiate(strongCorner, position, Quaternion.Euler(rotation),specifiedObject.transform);
                 myInstance.transform.SetParent(specifiedObject.transform);
             }
         }
 
-
-        int step = StrongWallLength;
-        // Front & Back facade (x axis)
-        for (var x = step; x < xWidth; x += step)
+        // ----------- Front & Back facade (x axis) -----------
+        
+        for (var x = StrongWallLength; x < xWidth; x += StrongWallLength)
         {
-            GameObject instance = GetFortressComponent(true, 0);
             
-            // Front facade
+            // ----------- Front facade -----------
+            GameObject instance = GetFortressComponent(true, 0);
             Vector3 position = new Vector3(specifiedObjectPosition.x + x, specifiedObjectPosition.y + heightOffset, specifiedObjectPosition.z);
             Vector3 rotation = new Vector3(rotationOffset, 90, 0);
 
@@ -126,11 +116,12 @@ public class CreateOutpost : MonoBehaviour
                 myInstance.transform.SetParent(specifiedObject.transform);
             }
             
-            // Back
+            // ----------- Back facade -----------
             instance = GetFortressComponent(true, 0);
             position = new Vector3(specifiedObjectPosition.x + x, specifiedObjectPosition.y + heightOffset, specifiedObjectPosition.z + zLength);
             rotation = new Vector3(rotationOffset, 90, 0);
             
+            // Make sure the position is not supposed to be the entrance
             if (!(selectedEntrance == EntrancePositionSelector.Back &&
                   ((xWidth / 2 - 6) <= x && x <= (xWidth / 2 + 6))))
             {
@@ -140,16 +131,18 @@ public class CreateOutpost : MonoBehaviour
         }
 
         
-        // Lateral facade (z axis)
-        for (var z = step; z < zLength; z += step)
+        // ----------- Lateral facade (z axis) -----------
+        
+        for (var z = StrongWallLength; z < zLength; z += StrongWallLength)
         {
+            // ----------- First facade -----------
             GameObject instance = GetFortressComponent(true, 0);
-            
-            // First facade
             Vector3 position = new Vector3(specifiedObjectPosition.x, specifiedObjectPosition.y + heightOffset, specifiedObjectPosition.z + z);
             Vector3 rotation = new Vector3(rotationOffset, 0, 0);
             
             GameObject myInstance;
+            
+            // Make sure the position is not supposed to be the entrance
             if (!(selectedEntrance == EntrancePositionSelector.Left &&
                   ((zLength / 2 - 6) <= z && z <= (zLength / 2 + 6))))
             {
@@ -157,11 +150,12 @@ public class CreateOutpost : MonoBehaviour
                 myInstance.transform.SetParent(specifiedObject.transform);
             }
 
+            // ----------- Second facade -----------
             instance = GetFortressComponent(true, 0);
             rotation = new Vector3(rotationOffset, 0, 0);
-            
-            // Second facade
             position = new Vector3(specifiedObjectPosition.x + xWidth, specifiedObjectPosition.y + heightOffset, specifiedObjectPosition.z + z);
+            
+            // Make sure the position is not supposed to be the entrance
             if (!(selectedEntrance == EntrancePositionSelector.Right &&
                   ((zLength / 2 - 6) <= z && z <= (zLength / 2 + 6))))
             {
@@ -173,75 +167,80 @@ public class CreateOutpost : MonoBehaviour
 
     private GameObject GetFortressComponent(bool isRandom, int assetNum)
     {
+        // Reset the offset variables to zero
         heightOffset = 0;
         rotationOffset = 0;
+        // Set idx to the provided asset number for cases where we don't need to generate a random one
         int idx = assetNum;
-
+        // Array of possible structures that can be generated
         int[] possibleRandomStructs = { SimpleWall, StrongWall, StrongWallDoor, Tower, DestructibleWall };
 
+        // If isRandom is set to true, get one of the indexes from possibleRandomStructs
         if (isRandom)
             idx = possibleRandomStructs[Random.Range(0, possibleRandomStructs.Length)];
 
+        // Calculate the offset variables according to the type of asset selected
         if (idx == Tower) heightOffset = 3.028076f;
         if (idx == SimpleWall) heightOffset = -0.7219238f;
-
-        if (idx == DestructibleWall) { rotationOffset = 90;
-            heightOffset = -0.3112183f;
-        }
+        if (idx == DestructibleWall) { rotationOffset = 90; heightOffset = -0.3112183f; }
             
         return assets[idx]; 
     }
 
-    void RepositionBuilding()
+    private void RepositionBuilding()
     {
-        /*float yOffset = 0;
-        float yVal = Terrain.activeTerrain.SampleHeight(new Vector3(specifiedObjectPosition.x, 0, specifiedObjectPosition.z));
-
-        //Apply Offset if needed
-        yVal += yOffset;
-
-        specifiedObject.transform.position = new Vector3(specifiedObjectPosition.x, yVal, specifiedObjectPosition.z);
-        */
         float yOffset = 4;
         specifiedObject.transform.position += new Vector3(0, yOffset, 0);
     }
 
-    void GenerateObstacles()
+    private void GenerateObstacles()
     {
-        float securityOffset = 8;
+        float securityOffset = 8;       // Distance between the obstacles and the borders of the fortress
+        float yPos = 0;                 // Initialization of the yPosition
+        Vector3 instancePos = default;  // Initialization of a dummy position vector
         
-        // Generate random x and z coordinates
-        float xPos = Random.Range(specifiedObjectPosition.x + securityOffset,
-            specifiedObjectPosition.x + xWidth - securityOffset);
-        float zPos = Random.Range(specifiedObjectPosition.z + securityOffset,
-            specifiedObjectPosition.z + zLength - securityOffset);
+        // While we haven't placed all the objects
+        for (int i = 0; i < numberOfObstacles; i++)
+        {
+            var foundValid = false;
+            // Search for a valid position
+            while (!foundValid)
+            {
+                // Generate random x and z coordinates
+                var xPos = Random.Range(specifiedObjectPosition.x + securityOffset,
+                    specifiedObjectPosition.x + xWidth - securityOffset);
+                var zPos = Random.Range(specifiedObjectPosition.z + securityOffset,
+                    specifiedObjectPosition.z + zLength - securityOffset);
 
-        Vector3 instancePos = new Vector3(xPos, 0, zPos);
-        float yPos = _terrains[GetClosestTerrain(instancePos)].SampleHeight(instancePos);
-        
-        
+                instancePos = new Vector3(xPos, 100, zPos);
+
+                // Raycast to see if there is anything on the ground or if the obstacle can be placed
+                RaycastHit hit;
+                if (Physics.Raycast(instancePos, transform.TransformDirection(Vector3.down), out hit, 100f,
+                        1 << LayerMask.NameToLayer("Ground")))
+                {
+                    yPos = hit.point.y;
+                    foundValid = true;
+                }
+
+            }
+            // Get the destructible wall instance
+            GameObject instance = GetFortressComponent(false, DestructibleWall);
+    
+            // Generate a random rotation and a position with the values from before
+            var rotation = new Vector3(0, Random.Range(0, 360), 0);
+            var position = new Vector3(instancePos.x, yPos + 2, instancePos.z);
+    
+            // Create the instance and set its parent as the specified object
+            var myInstance = Instantiate(instance, position, Quaternion.Euler(rotation), specifiedObject.transform);
+            myInstance.transform.SetParent(specifiedObject.transform);
+
+        }
             
         
+        
+        
     }
-    int GetClosestTerrain(Vector3 positionToBeChecked)
-    {
-        int terrainIndex = 0;
-        float lowDist = float.MaxValue;
-
-        for (int i = 0; i < _terrains.Length; i++)
-        {
-            var center = new Vector3(_terrains[i].transform.position.x + _terrains[i].terrainData.size.x / 2, positionToBeChecked.y, _terrains[i].transform.position.z + _terrains[i].terrainData.size.z / 2);
-
-            float dist = Vector3.Distance(center, positionToBeChecked);
-
-            if (dist < lowDist)
-            {
-                lowDist = dist;
-                terrainIndex = i;
-            }
-        }
-        return terrainIndex;
-    }
-
+    
 }
 
